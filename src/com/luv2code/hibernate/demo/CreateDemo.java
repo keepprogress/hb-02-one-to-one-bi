6cf86c8a4e9e78f51ba7edc34daa61de7f7042c4
getzch <keepprogress@gmail.com>
2020-12-06 09:51:33 +0800

    delete InstructorDetail from constructor in Instructor.java. create Main App

--- src/com/luv2code/hibernate/demo/CreateDemo.java
+++ src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -4,33 +4,37 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
 import com.luv2code.hibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class CreateDemo {
 
 	public static void main(String[] args) {
 
 		// create session factory
 		SessionFactory factory = new Configuration()
 								.configure("hibernate.cfg.xml")
-								.addAnnotatedClass(Student.class)
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
 								.buildSessionFactory();
 		
 		// create session
 		Session session = factory.getCurrentSession();
 		
 		try {
-			// create a student object
-			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Paul", "Wall", "paul@luv2code.com");
+					
+			// create the objects
+			Instructor tempInstructor =
+					new Instructor("Chad", "Darby", "darby@luv2code.com");
+			
+			
+			// associate the objects
 			
 			// start a transaction
 			session.beginTransaction();
 			
-			// save the student object
-			System.out.println("Saving the student...");
-			session.save(tempStudent);
-			
+						
 			// commit transaction
 			session.getTransaction().commit();
 			
