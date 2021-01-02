0ae211d3dedd521065a17cbdcd85110786780c14
getzch <keepprogress@gmail.com>
2021-01-03 01:17:06 +0800

    update

--- src/com/luv2code/hibernate/demo/entity/InstructorDetail.java
+++ src/com/luv2code/hibernate/demo/entity/InstructorDetail.java
@@ -40,8 +40,9 @@ public class InstructorDetail {
 	
 	// add @OnetoOne annotation
 	
-	@OneToOne(mappedBy="instructorDetail", cascade= {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
-														CascadeType.REFRESH})
+	@OneToOne(mappedBy="instructorDetail", 
+			cascade= {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+						CascadeType.REFRESH})
 	private Instructor instructor;
 	
 	
